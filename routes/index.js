52ec9c7e61d3913c6fc7a620fe0f2d3932521c49
MarkCBall <moball@hotmail.com>
2018-10-20 13:49:42 -0400

    revamped structure

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Blog Post App',
-    app: {
-      message: 'Welcome to my blog post app',
-    }
-  });
+  res.render('index', {});
+});
+
+router.get('/:slug',function(req, res, next) {
+    //filter out only slug's portfolios and display as well
+    res.render('postDisp', {path1: req.params.slug} ); 
+});
+
+router.get('/:slug/:slug2',function(req, res, next) {
+    //filter out only slug's portfolios and display as well
+    res.render('coinsDisp', {path1: req.params.slug, path2:req.params.slug2} );
 });
 
 module.exports = router;
