4f7827b75d9243e25725c796170f9468822295fe
MarkCBall <moball@hotmail.com>
2018-10-20 14:06:34 -0400

    reverting what I pushed by mistake

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {});
-});
-
-router.get('/:slug',function(req, res, next) {
-    //filter out only slug's portfolios and display as well
-    res.render('postDisp', {path1: req.params.slug} ); 
-});
-
-router.get('/:slug/:slug2',function(req, res, next) {
-    //filter out only slug's portfolios and display as well
-    res.render('coinsDisp', {path1: req.params.slug, path2:req.params.slug2} );
+  res.render('index', {
+    title: 'Blog Post App',
+    app: {
+      message: 'Welcome to my blog post app',
+    }
+  });
 });
 
 module.exports = router;
